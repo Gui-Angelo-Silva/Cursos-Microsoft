8233f2e9f72b4fa78bb26fab451972f1f71dc58e
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-08-01 11:34:46 -0300

    ContosoPizza: Cadastrar pedido de pizza, curso Microsoft

--- mslearn-create-razor-pages-aspnet-core-main/ContosoPizza/Pages/PizzaList.cshtml.cs
+++ mslearn-create-razor-pages-aspnet-core-main/ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -7,6 +7,8 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
@@ -17,6 +19,17 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
         }
     }
 }
