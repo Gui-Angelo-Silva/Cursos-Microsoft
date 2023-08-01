db7a60f9062a2ff992504aa5eec1f0a477072ec5
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-08-01 10:17:09 -0300

    ContosoPizza: Adicionando título, curso Microsoft

---
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPizza.Pages
+{
+    public class PizzaListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
